c9fe612ff7dad03b2f70a4153858b351e791bac5
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-10-04 12:34:11 -0500

    Made a Function that take in a array and adds

---
+++ Tic-Tac-Toe/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                         
+                                                             /*0  1  2*/
+            int[,] array2DInitExample = new int[3, 3] { /*0*/{ 1, 2, 3 }, 
+                                                        /*1*/{ 4, 5, 6 }, 
+                                                        /*2*/{ 7, 8, 9 }};
+            int[,] array2DInitExample2 = { { 1, 2, 3 }, 
+                                           { 4, 5, 6 }, 
+                                           { 7, 8, 9 }, 
+                                           { 10, 11, 12 } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(array2DInitExample[i, j]);                    
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
+            void Twodmath(int[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    int sum = 0;
+                    Console.WriteLine();
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        sum += array[i, j];                        
+                    }
+                    Console.WriteLine(sum);
+                }
+            }
+            Twodmath(array2DInitExample);
+        }
+    }
+}
