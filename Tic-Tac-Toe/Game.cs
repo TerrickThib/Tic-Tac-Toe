b2660d780e7100959eab2286f17451af791c09d4
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-10-04 16:56:08 -0500

    Created a board

--- Tic-Tac-Toe/Game.cs
+++ Tic-Tac-Toe/Game.cs
@@ -6,5 +6,67 @@ namespace Tic_Tac_Toe
 {
     class Game
     {
+        //Varibles
+        private static bool _gameOver = false;
+        private Board _gameBoard;
+
+        /// <summary>
+        /// Begings the game
+        /// </summary>
+        public void Run()
+        {
+            Start();
+
+            while (!_gameOver)
+            {
+                Draw();
+                Update();
+            }
+
+            End();
+        }
+
+        /// <summary>
+        /// Called when the game starts
+        /// </summary>
+        private void Start()
+        {
+            _gameBoard = new Board();
+            _gameBoard.Start();
+        }
+
+        /// <summary>
+        /// Called everytime the game loops
+        /// </summary>
+        private void Update()
+        {
+            _gameBoard.Update();
+        }
+
+        /// <summary>
+        /// Updates the display to reflect any changes made while running
+        /// </summary>
+        private void Draw()
+        {
+            Console.Clear();
+            _gameBoard.Draw();            
+        }
+
+        /// <summary>
+        /// Ends the game and closes application
+        /// </summary>
+        private void End()
+        {
+            _gameBoard.End();
+        }
+
+        public static int GetInput()
+        {
+            int choice = -1;            
+            if (!int.TryParse(Console.ReadLine(), out choice))
+                choice = -1;
+
+            return choice;
+        }
     }
 }
--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -5,8 +5,9 @@ namespace Tic_Tac_Toe
     class Program
     {
         static void Main(string[] args)
-        {                         
-              
+        {
+            Game game = new Game();            
+            game.Run();;
         }
     }
 }
